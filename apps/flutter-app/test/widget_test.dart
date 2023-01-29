edfd5b5f8978d0a4d99ea75b71fb19db8fad0e5e
Vngdv <13379212+Vngdv@users.noreply.github.com>
2023-01-29 02:40:06 +0100

    test(flutter-app): add basic functionality test

--- apps/flutter-app/test/widget_test.dart
+++ apps/flutter-app/test/widget_test.dart
@@ -5,9 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cerulean_app/main.dart';
-
-void main() {}
+void main() {
+  test("Basic test", () {
+    expect(1, 1);
+  });
+}
