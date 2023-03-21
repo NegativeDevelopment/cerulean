31acec2e4a13b6f144184cd265e97b3c7716ea0f
Vngdv <13379212+Vngdv@users.noreply.github.com>
2023-03-21 15:37:51 +0100

    feat: basic login

--- apps/flutter_app/lib/src/auth/auth_service.dart
+++ apps/flutter_app/lib/src/auth/auth_service.dart
@@ -1,6 +1,26 @@
+import 'dart:convert';
+
+import 'package:flutter_app/src/constants.dart';
+import 'package:http/http.dart' as http;
+
 class AuthService {
   Future<String> login(String username, String password) async {
-    return "";
+    var response = await http.post(
+      Uri.parse('$appApiBaseUrl/auth/login'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'username': username,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)['token'];
+    } else {
+      throw Exception('Failed to Login');
+    }
   }
 
   Future<bool> register(String username, String password) async {
